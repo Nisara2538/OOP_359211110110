66f38803df7131d8874509083ecc3ebed90c5b5e
Nisara.bobo@gmail.com <b0901544021>
2018-01-26 09:23:36 +0700

    ooplab7

---
+++ src/sample/ooplab7/StudentApp.java
@@ -0,0 +1,11 @@
+package sample.ooplab7;
+
+import sun.security.ec.ECDSASignature;
+
+public class StudentApp {
+    private static void main (String)[]
+        //create object as Student
+        Student S1 = new Student();
+        S1.setId ("0001")ว
+        S1.
+}
